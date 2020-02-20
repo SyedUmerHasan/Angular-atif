3bca3df3a2ec84450c5aae752ff2921e806d9140
syedumerhasan <umerhasan2010@gmail.com>
2020-02-21 01:10:54 +0500

    checking code 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [DynamicScriptLoaderService]
 })
 export class AppModule { }
