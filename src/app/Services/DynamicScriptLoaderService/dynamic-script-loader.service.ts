64ee14bef5620083dabbb77f4a9e4b188e7a2a06
syedumerhasan <umerhasan2010@gmail.com>
2020-02-22 18:10:16 +0500

    dynamic leading

--- src/app/Services/DynamicScriptLoaderService/dynamic-script-loader.service.ts
+++ src/app/Services/DynamicScriptLoaderService/dynamic-script-loader.service.ts
@@ -6,27 +6,27 @@ interface Scripts {
 }
 
 export const ScriptStore: Scripts[] = [
-  { name: "1", src : "/src/assets/usertheme/js/jquery-3.2.1.min.js"},
-  { name: "2", src : "/src/assets/usertheme/js/propper.js"},
-  { name: "3", src : "/src/assets/usertheme/js/bootstrap.min.js"},
-  { name: "4", src : "/src/assets/usertheme/vendors/wow/wow.min.js"},
-  { name: "5", src : "/src/assets/usertheme/vendors/sckroller/jquery.parallax-scroll.js"},
-  { name: "6", src : "/src/assets/usertheme/vendors/owl-carousel/owl.carousel.min.js"},
-  { name: "7", src : "/src/assets/usertheme/vendors/imagesloaded/imagesloaded.pkgd.min.js"},
-  { name: "8", src : "/src/assets/usertheme/vendors/isotope/isotope-min.js"},
-  { name: "9", src : "/src/assets/usertheme/vendors/magnify-pop/jquery.magnific-popup.min.js"},
-  { name: "10", src : "/src/assets/usertheme/vendors/bootstrap-selector/js/bootstrap-select.min.js"},
-  { name: "11", src : "/src/assets/usertheme/vendors/nice-select/jquery.nice-select.min.js"},
-  { name: "12", src : "/src/assets/usertheme/vendors/scroll/jquery.mCustomScrollbar.concat.min.js"},
-  { name: "13", src : "/src/assets/usertheme/js/plugins.js"},
-  { name: "14", src : "/src/assets/usertheme/js/main.js"},
-  { name:"15", src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyB13ZAvCezMx5TETYIiGlzVIq65Mc2FG5g"},
-  { name:"16", src: "/src/assets/usertheme/js/gmaps.min.js"},
-  { name:"17", src: "/src/assets/usertheme/js/plugins.js"},
-  { name:"18", src: "/src/assets/usertheme/js/jquery.form.js"},
-  { name:"19", src: "/src/assets/usertheme/js/jquery.validate.min.js"},
-  { name:"20", src: "/src/assets/usertheme/js/contact.js"},
-  { name:"21", src: "/src/assets/usertheme/js/main.js"}
+  { name: "1", src : "/assets/usertheme/js/jquery-3.2.1.min.js"},
+  { name: "2", src : "/assets/usertheme/js/propper.js"},
+  { name: "3", src : "/assets/usertheme/js/bootstrap.min.js"},
+  { name: "4", src : "/assets/usertheme/vendors/wow/wow.min.js"},
+  { name: "5", src : "/assets/usertheme/vendors/sckroller/jquery.parallax-scroll.js"},
+  { name: "6", src : "/assets/usertheme/vendors/owl-carousel/owl.carousel.min.js"},
+  { name: "7", src : "/assets/usertheme/vendors/imagesloaded/imagesloaded.pkgd.min.js"},
+  { name: "8", src : "/assets/usertheme/vendors/isotope/isotope-min.js"},
+  { name: "9", src : "/assets/usertheme/vendors/magnify-pop/jquery.magnific-popup.min.js"},
+  { name: "10", src : "/assets/usertheme/vendors/bootstrap-selector/js/bootstrap-select.min.js"},
+  { name: "11", src : "/assets/usertheme/vendors/nice-select/jquery.nice-select.min.js"},
+  { name: "12", src : "/assets/usertheme/vendors/scroll/jquery.mCustomScrollbar.concat.min.js"},
+  { name: "13", src : "/assets/usertheme/js/plugins.js"},
+  { name: "14", src : "/assets/usertheme/js/main.js"},
+  { name:"15", src: "//maps.googleapis.com/maps/api/js?key=AIzaSyB13ZAvCezMx5TETYIiGlzVIq65Mc2FG5g"},
+  { name:"16", src: "/assets/usertheme/js/gmaps.min.js"},
+  { name:"17", src: "/assets/usertheme/js/plugins.js"},
+  { name:"18", src: "/assets/usertheme/js/jquery.form.js"},
+  { name:"19", src: "/assets/usertheme/js/jquery.validate.min.js"},
+  { name:"20", src: "/assets/usertheme/js/contact.js"},
+  { name:"21", src: "/assets/usertheme/js/main.js"}
 ];
 
 
